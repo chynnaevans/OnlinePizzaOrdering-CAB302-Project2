3ac2107bb1aadb3b9c8498fc1a45ad8eae79ef4d
owlbewb <danielabreu96@gmail.com>
2017-05-25 00:17:54 +1000

    started junit for LogHandler()

--- cab302-asgn2-7dbe90423b68/src/asgn2Tests/LogHandlerCustomerTests.java
+++ cab302-asgn2-7dbe90423b68/src/asgn2Tests/LogHandlerCustomerTests.java
@@ -1,10 +1,26 @@
 package asgn2Tests;
 
+import java.util.ArrayList;
+import org.junit.Test;
+import asgn2Customers.*;
+import asgn2Restaurant.*;
+import asgn2Exceptions.*;
+
 /**
  * A class that tests the methods relating to the creation of Customer objects in the asgn2Restaurant.LogHander class.
  *
  * @author Person A
  */
 public class LogHandlerCustomerTests {
-	// TO DO
+	
+	@Test
+	public void TestPopulateCustomerDataset_Correct() throws CustomerException, LogHandlerException{
+		ArrayList<Customer> test = new ArrayList<Customer>();
+		test.add(CustomerFactory.getCustomer("DVC", "Name", "0123456789", 2, 2));
+		
+		assert(test == LogHandler.populateCustomerDataset("customerTest.txt"));
+		
+	}
+	
+	
 }
